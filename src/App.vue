dfc1b38c7fe59db18085c881d29b2f5bcc33eabc
Xime <ximena.ruiz@cceo.com.mx>
2026-09-03 11:00:32 -0600

    feat (Array): ✨implement list rendering with v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
 const name = "Vue dinámico";
-const styleColor = "color: blue";
-const arrayColores = ["blue", "red", "peru"];
-const activo = false;
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCase() }}</h1>
-  <h2>{{ arrayColores }}</h2>
-  <h2 :style="`color: ${arrayColores[2]}`">Soy Perú</h2>
-  <h2 v-if="activo === true">
-    <span>Icono</span>
-    Estoy activo
-  </h2>
-
-  <p v-else-if="activo === false">Estoy inactivo</p>
-  <p v-else>Estoy indeciso</p>
-
-  <h2 v-show="activo">Estoy activo v-show</h2>
+  <ul>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">
+      {{ index }} - {{ fruta }}
+    </li>
+  </ul>
 </template>
 
 <style>
