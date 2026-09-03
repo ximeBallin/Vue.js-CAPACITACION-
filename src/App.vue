b8ee7f2b730c62392827a8355f07593e0ddf8ca0
Xime <ximena.ruiz@cceo.com.mx>
2026-09-03 10:43:38 -0600

    feat (JS):✨add array colors, methods in templates, and ternary operators using JS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 const name = "Vue dinámico";
 const styleColor = "color: blue";
+const arrayColores = ["blue", "red", "peru"];
+const activo = false;
 </script>
 
 <template>
-  <h1>Hola {{ name }}</h1>
-  <h2 :style="styleColor">Soy azul</h2>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <h2>{{ arrayColores }}</h2>
+  <h2 :style="`color: ${arrayColores[2]}`">Soy Perú</h2>
+  <h2>
+    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
+  </h2>
 </template>
 
 <style>
