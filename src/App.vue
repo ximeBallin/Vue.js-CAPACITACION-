6b4e653b74697911f2a08b2de48c9e3d9fd30464
Xime <ximena.ruiz@cceo.com.mx>
2026-09-04 09:28:34 -0600

    feat (Bootstrap):✨implement dynamic counter with favorites list and bootstrap styles

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed } from 'vue';
 
 const name = "Vue dinámico";
 
 const counter = ref(0);
-const arrayFavoritos = ref([]);
+const arrayFavoritos = ref([])
 
-const increment = () => counter.value++;
-const decrement = () => counter.value--;
+const increment = () => {
+  counter.value++;
+};
+
+const decrement = () => {
+  counter.value--;
+};
 
 const reset = () => {
   counter.value = 0;
 };
+
+const add = () => {
+  arrayFavoritos.value.push(counter.value)
+}
+
+const bloquearBtnAdd = computed(() => {
+  const numSearch = arrayFavoritos.value.find((num) => num === counter.value);
+  console.log(numSearch);
+  if(numSearch === 0) return true;
+  return numSearch ? true :false ;
+  //return numSearch || numSearch === 0;
+});
 
 const classCounter = computed(() => {
   if (counter.value === 0) {
@@ -24,37 +41,36 @@ const classCounter = computed(() => {
     return 'negative';
   }
 });
-
-const add = () => {
-  arrayFavoritos.value.push(counter.value);
-};
-
-const bloquearBtnAdd = computed(() => {
-  const numSearch = arrayFavoritos.value.find(num => num === counter.value);
-  console.log(numSearch);
-  if(numSearch === 0) return true;
-  return numSearch ? true : false;
-  //return numSearch || numSearch === 0
-});
 </script>
 
 <template>
-  <h1>Hola {{ name.toUpperCase() }}</h1>
-  <h2 :class="classCounter">{{ counter }}</h2>
-  <button @click="increment">Incremento</button>
-  <button @click="decrement">Decremento</button>
-  <button @click="reset">Reset</button>
-  <button @click="add" :disabled="bloquearBtnAdd">Add</button>
-  <br>
-  {{ arrayFavoritos }}
-  <ul>
-    <li
-        v-for="(num, index) in arrayFavoritos"
-        :key="index"
-    >
-      {{ num }}
-    </li>
-  </ul>
+  <div class="container text-center mt-3">
+    <h1>Hola {{ name.toUpperCase() }}</h1>
+    <h2 :class="classCounter">{{ counter }}</h2>
+    <div class="btn-group">
+      <button @click="increment" class="btn btn-success">
+        Increment
+      </button>
+      <button @click="decrement" class="btn btn-danger">Decrement</button>
+      <button @click="reset" class="btn btn-secondary">Reset</button>
+      <button
+          @click="add"
+          :disabled="bloquearBtnAdd"
+          class="btn btn-primary"
+      >
+        Add
+      </button>
+    </div>
+    <ul class="list-group mt-4">
+      <li
+          class="list-group-item"
+          v-for="(num, index) in arrayFavoritos"
+          :key="index"
+      >
+        {{ num }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style>
