2d641474e96aec952e428d5b0664e6cec55b73eb
Xime <ximena.ruiz@cceo.com.mx>
2026-09-04 16:16:47 -0600

    feat (Referencias-Reactivas):✨wire up pagination events and implement slice navigation with postXpage

--- vue-project-2/src/App.vue
+++ vue-project-2/src/App.vue
@@ -1,33 +1,52 @@
 <script setup>
 import ButtonCounter from "./components/ButtonCounter.vue";
 import BlogPost from "./components/BlogPost.vue";
+import PaginatePost from "./components/PaginatePost.vue";
 import { ref } from "vue";
-import PaginatePost from "@/components/PaginatePost.vue";
 
-const posts = ref([
-  { title: "Post 1", id: 1, body: "descripción 1" },
-  { title: "Post 2", id: 2, body: "descripción 2" },
-  { title: "Post 3", id: 3, body: "descripción 3" },
-  { title: "Post 4", id: 4 },
-]);
+const posts = ref([]);
+const postXpage = 5;
+const inicio = ref(0);
+const fin = ref(postXpage);
+
 const favorito = ref("");
 const cambiarFavorito = (title) => {
   favorito.value = title;
 };
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then((data) => { posts.value = data });
+const siguiente = () => {
+  inicio.value = inicio.value + postXpage;
+  fin.value = fin.value + postXpage;
+};
+
+const anterior = () => {
+  if (inicio.value > 0) {
+    inicio.value = inicio.value - postXpage;
+    fin.value = fin.value - postXpage;
+  }
+};
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      posts.value = data;
+    });
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
     <h2>Mis Post Favoritos: {{ favorito }}</h2>
-    <PaginatePost class="mb-2"></PaginatePost>
+
+    <!-- Pasamos las funciones como props o eventos -->
+    <PaginatePost
+        class="mb-2"
+        @siguiente="siguiente"
+        @anterior="anterior"
+    ></PaginatePost>
 
     <BlogPost
-        v-for="post in posts.slice(0, 3)"
+        v-for="post in posts.slice(inicio, fin)"
         :key="post.id"
         :title="post.title"
         :id="post.id"
