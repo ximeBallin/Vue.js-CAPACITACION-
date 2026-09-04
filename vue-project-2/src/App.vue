204171a4f4e4ce2e7629b4a61583dd9a595d5052
Xime <ximena.ruiz@cceo.com.mx>
2026-09-04 10:51:18 -0600

    feat (Primer-Componente):✨add counter logic and increment event to App.vue

--- vue-project-2/src/App.vue
+++ vue-project-2/src/App.vue
@@ -1,8 +1,28 @@
-<script setup></script>
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    const counter = ref(0);
+
+    const increment = ()=>{
+      counter.value ++
+    }
+
+    return{
+      counter,
+      increment
+    };
+
+  },
+}
+</script>
+
+<!--<script setup ></script> -->
 
 
 <template>
   <h1>APP</h1>
+  <button @click="increment">{{counter}}</button>
 
 </template>
 
