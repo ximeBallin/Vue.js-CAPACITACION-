717ad4f4d5f2cfe10b9322b4b57c9e4acb16fa33
Xime <ximena.ruiz@cceo.com.mx>
2026-09-04 12:16:24 -0600

    feat (Multiples-Props-2):✨add id and dynamic colorText props to BlogPost component

--- vue-project-2/src/components/BlogPost.vue
+++ vue-project-2/src/components/BlogPost.vue
@@ -1,12 +1,14 @@
 <script setup>
 
-defineProps(['title'])
+defineProps(["title","id","body","colorText"])
 </script>
 
 <template>
   <div class="card">
     <div class ="card-body">
-      <h5 class="card-title">{{title}}</h5>
+      <h5 class="card-title"
+      :class="`text-${colorText}`"
+      >{{id}}-{{title}}</h5>
       <p>Lorem ipsum dolor sit amet.</p>
 
     </div>
