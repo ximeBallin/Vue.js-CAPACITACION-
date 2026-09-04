a35d3a246815d8c5a11a3b3ab7a213a96615521c
Xime <ximena.ruiz@cceo.com.mx>
2026-09-04 12:01:23 -0600

    feat (Multiples-Props):✨implement BlogPost component with props and card layout

---
+++ vue-project-2/src/components/BlogPost.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+defineProps(['title'])
+</script>
+
+<template>
+  <div class="card">
+    <div class ="card-body">
+      <h5 class="card-title">{{title}}</h5>
+      <p>Lorem ipsum dolor sit amet.</p>
+
+    </div>
+
+  </div>
+</template>
