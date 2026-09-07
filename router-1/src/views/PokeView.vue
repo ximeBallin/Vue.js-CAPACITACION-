8f11536e12d8930349621c5707a9454cad91037d
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 13:28:33 -0600

    feat (Agregando-Errores):✨add error handling and alert styling to PokemonsView template

--- router-1/src/views/PokeView.vue
+++ router-1/src/views/PokeView.vue
@@ -5,7 +5,7 @@ import { useGetData } from "@/composables/getData";
 const route = useRoute();
 const router = useRouter();
 
-const { getData, data, loading } = useGetData();
+const { getData, data, loading, error } = useGetData();
 
 const back = () => {
   router.push("/pokemons");
@@ -16,10 +16,10 @@ getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
 
 <template>
   <p v-if="loading">Cargando información...</p>
+  <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
   <div v-if="data">
     <img :src="data.sprites?.front_default" alt="" />
     <h1>Poke name: {{ $route.params.name }}</h1>
   </div>
-  <h1 v-else>No existe el pokemon</h1>
   <button @click="back" class="btn btn-outline-primary">Volver</button>
 </template>
