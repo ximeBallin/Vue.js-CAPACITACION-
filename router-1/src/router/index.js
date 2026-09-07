0acdd1e8475f870a3b19f43f07c01e4611733b18
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 12:50:26 -0600

    feat (Ruta-404):✨verify router configuration and test 404 catch-all route correctly

--- router-1/src/router/index.js
+++ router-1/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: "active",
   routes: [
     {
       path: '/',
@@ -22,7 +23,12 @@ const router = createRouter({
       path: '/pokemones/:name',
       name: 'poke',
       component: () => import('../views/PokeView.vue')
-    }
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("../views/NotFoundoView.vue"),
+    },
   ]
 })
 
