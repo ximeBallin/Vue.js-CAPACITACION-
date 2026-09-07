caebda4f89e601bee080d1ca1129ff97408e9c56
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 11:10:33 -0600

    feat (Rutas-Dinamicas):✨render pokemon list using v-for and router-link

--- router-1/src/App.vue
+++ router-1/src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">Pokemons</RouterLink>
+        <RouterLink to="/pokemones">Pokemons</RouterLink>
       </nav>
     </div>
   </header>
