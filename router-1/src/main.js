5f05744d3ea1719f45875a3ee2ff993256f06308
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 15:24:09 -0600

    feat (Instalacion-Proyecto-Existente):✨initialize useCounterStore definition with Pinia

--- router-1/src/main.js
+++ router-1/src/main.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-const app = createApp(App)
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(router)
+app.use(router);
+app.use(pinia);
 
-app.mount('#app')
+app.mount("#app");
