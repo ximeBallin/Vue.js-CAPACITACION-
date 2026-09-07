5c0c12b887562d480a2e40abb76a34adef83282d
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 15:43:06 -0600

    feat (Desestructuracion):✨destructure store state and getters using storeToRefs in HomeView

--- router-1/src/views/HomeView.vue
+++ router-1/src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { useCounterStore } from "@/store/counter.js";
+import { storeToRefs } from "pinia";
 
 const useCounter = useCounterStore();
+
+const { increment } = useCounter;
+const { count, double } = storeToRefs(useCounter);
 </script>
 
 <template>
-  <h1>Home counter: {{ useCounter.count }}</h1>
-  <h2>Double: {{ useCounter.double }}</h2>
-  <button @click="useCounter.increment">Increment</button>
+  <h1>Home counter: {{ count }}</h1>
+  <h2>Double: {{ double }}</h2>
+  <button @click="increment">Increment</button>
 </template>
