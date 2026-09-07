d0654da187341e46334f811ecb7f278bd4f990f7
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 13:19:42 -0600

    feat (Reutilizando-Composable):✨implement dynamic pokemon detail fetching with loading and error handling in PokeView

--- router-1/src/views/PokeView.vue
+++ router-1/src/views/PokeView.vue
@@ -1,34 +1,23 @@
 <script setup>
-import axios from "axios";
-import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
+import { useGetData } from "@/composables/getData";
 
 const route = useRoute();
 const router = useRouter();
-const poke = ref({});
+
+const { getData, data, loading } = useGetData();
 
 const back = () => {
-  router.push('/pokemones');
-}
-
-const getData = async () => {
-  try {
-    const { data } = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
-    );
-    console.log(data);
-    poke.value = data;
-  } catch (error) {
-    console.log(error);
-  }
+  router.push("/pokemons");
 };
 
-getData();
+getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
 </script>
 
 <template>
-  <div v-if="poke">
-    <img :src="poke.sprites?.front_default" alt="" />
+  <p v-if="loading">Cargando información...</p>
+  <div v-if="data">
+    <img :src="data.sprites?.front_default" alt="" />
     <h1>Poke name: {{ $route.params.name }}</h1>
   </div>
   <h1 v-else>No existe el pokemon</h1>
