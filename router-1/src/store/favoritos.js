f3b37904e96855998d0d0bf814735cffd6d9e336
Xime <ximena.ruiz@cceo.com.mx>
2026-09-07 18:05:06 -0600

    feat (Bloquear-Boton):✨add findPoke method and disable add button when pokemon is already favorited

--- router-1/src/store/favoritos.js
+++ router-1/src/store/favoritos.js
@@ -12,9 +12,13 @@ export const useFavoritosStore = defineStore("favoritos", () => {
         favoritos.value = favoritos.value.filter((item) => item.id !== id);
     };
 
+    const findPoke = (name) =>
+        favoritos.value.find((item) => item.name === name);
+
     return {
         favoritos,
         add,
         remove,
+        findPoke,
     };
 });
